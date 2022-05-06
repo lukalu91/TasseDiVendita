07efa216c6fc6827c8a9d4bae939b2cd9b40efdc
Angela <Angela@LAPTOP-J3NNPGPI.fritz.box>
2022-05-06 10:36:39 +0200

    + javadocs

---
+++ src/test/java/PrimoEsercizioTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PrimoEsercizioTest {
+	 	@Test
+	    void testGet() {
+	        assertEquals("1", "1");
+	    }
+}
---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.lbriganti.tassedivendita.bean"},{"l":"it.lbriganti.tassedivendita.factory"}];updateSearchResults();
